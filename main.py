0d1bc98e3cc0c2424fbdb942d3c9c53300a8daca
tamaspenzes-2001 <penzestamas@proton.me>
2024-05-07 15:48:07 +0200

    implement option to add new item

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 from beaupy import confirm, prompt, select, select_multiple
 from rich.console import Console
 import sys
+import os
 
 console = Console()
 
 def add_item():
-  pass
+  new_item = ""
+  while new_item == "":
+    new_item = prompt("New item:").strip()
+  with open("todos.txt", "a") as todos_doc:
+    todos_doc.write("\n" + new_item)
 
 def choose_todo_action(selected_todo):
   pass
 
 def main():
-  console.print("[green underline]YOUR TODOS:[/green underline]")
   while True:
+    os.system('cls' if os.name=='nt' else 'clear')
+    console.print("[green underline]YOUR TODOS:[/green underline]")
     todos = []
     with open("todos.txt") as todos_doc:
-      for todo in todos_doc:
-        todos.append(todo.strip())
+      for index, todo in enumerate(todos_doc, start=1):
+        todos.append(str(index) + ". " + todo.strip())
     todos.append("[cyan]Add item[/cyan]")
     todos.append("[red]Quit[/red]")
-    # print("\n\x1b[92mCOMMANDS:\x1b[0m")
-    # print("add [todo text]: add a todo")
-    # print("quit: quit app")
-    # user_choice = input("> ")
     selected_todo = select(todos)
+    # Keep select items visible after selecting an option
+    for todo in todos[:-2]:
+      print(todo)
     match selected_todo:
       case "[cyan]Add item[/cyan]": add_item()
       case "[red]Quit[/red]":

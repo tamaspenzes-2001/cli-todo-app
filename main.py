7de970a8da118f68e87284bf84f773a5fca2abdb
tamaspenzes-2001 <penzestamas@proton.me>
2024-05-07 13:48:07 +0200

    implement menu

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
-with open("todos.txt") as todos_doc:
-  for todo in todos_doc:
-    print(todo.strip())
+from beaupy import confirm, prompt, select, select_multiple
+from rich.console import Console
+import sys
+
+console = Console()
+
+def add_item():
+  pass
+
+def choose_todo_action(selected_todo):
+  pass
+
+def main():
+  console.print("[green underline]YOUR TODOS:[/green underline]")
+  while True:
+    todos = []
+    with open("todos.txt") as todos_doc:
+      for todo in todos_doc:
+        todos.append(todo.strip())
+    todos.append("[cyan]Add item[/cyan]")
+    todos.append("[red]Quit[/red]")
+    # print("\n\x1b[92mCOMMANDS:\x1b[0m")
+    # print("add [todo text]: add a todo")
+    # print("quit: quit app")
+    # user_choice = input("> ")
+    selected_todo = select(todos)
+    match selected_todo:
+      case "[cyan]Add item[/cyan]": add_item()
+      case "[red]Quit[/red]":
+        if confirm("Are you sure you want to quit?"):
+          sys.exit()
+      case _: choose_todo_action(selected_todo)
+
+main()
